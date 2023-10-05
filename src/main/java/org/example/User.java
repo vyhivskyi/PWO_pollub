93549e5c20ee20573af804662ae3c6fb588d2ffa
student <jan.kowalski@pollub.edu.pl>
2023-10-05 15:29:22 +0200

    feat/pollub-2

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class User {
+    public void addUser (String name) {
+        System.out.println(name);
+    }
+    public void setUser() {
+        System.out.println("name");
+    }
+
+}
